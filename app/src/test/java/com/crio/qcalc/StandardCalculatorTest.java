a66f97af38a259954c11c09e113d779dd703b322
root <root@simran-crio>
2023-03-20 21:30:46 +0550

    first commit

---
+++ app/src/test/java/com/crio/qcalc/StandardCalculatorTest.java
@@ -0,0 +1,71 @@
+package com.crio.qcalc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import com.crio.qcalc.StandardCalculator;
+
+public class StandardCalculatorTest {
+    private StandardCalculator standardCalculator;
+
+    @BeforeEach
+    void setup(){
+        standardCalculator = new StandardCalculator();
+    }
+
+    @Test
+    @DisplayName("Test Multiplication Overflow of Two Doubles")
+    void testMultiplicationOverflowForDoubles(){
+        //Assert
+        Assertions.assertThrows(ArithmeticException.class,new Executable(){
+            @Override
+            public void execute() throws Throwable{
+                standardCalculator.multiply(Double.MAX_VALUE,Double.MAX_VALUE);
+            }
+        });
+    }
+    @Test
+    @DisplayName("Test Multiplication Overflow of Two Doubles in which One is Positive and Other is Negative")
+    void testMultiplicationOverflowForDoublesOnePosOtherNeg(){
+        //Assert
+        Assertions.assertThrows(ArithmeticException.class,new Executable(){
+            @Override
+            public void execute() throws Throwable{
+                standardCalculator.multiply(-Double.MAX_VALUE,Double.MAX_VALUE);
+            }
+        });
+    }
+    
+    @Test
+    @DisplayName("Test Division Divide By Zero Scenario")
+    void testDivisionDivideByZero(){
+        //Assert
+        Assertions.assertThrows(ArithmeticException.class,new Executable(){
+            @Override
+            public void execute() throws Throwable{
+                standardCalculator.divide(10,0);
+            }
+        });
+    }
+
+ 
+
+    @Test
+    @DisplayName("Test Addition of Two Integers")
+    void testAdditionOperation(){
+        standardCalculator.add(1,1);
+        Assertions.assertEquals(2, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Substraction of Two Integers")
+    void testSubtractionOperation(){
+        standardCalculator.subtract(1,1);
+        Assertions.assertEquals(0, actualResult);
+    }
+
+
+}
